15578574bc55e0e3705665d79a7aa3d9c26bffaa
Calvin Ference <calvin.ference@linux.com>
2016-08-16 15:27:55 -0400

    Restructured

--- d20sim/__main__0.py
+++
@@ -1,6 +0,0 @@
-# Copyright (C) Mandalorian Mercs Costume Club - All Rights Reserved
-# Unauthorized copying of this file, via any medium is strictly prohibited
-# Proprietary and confidential
-# Written by Calvin Ference <calvin.ference@gmail.com>, July 2016
-from .oas import main
-main()
